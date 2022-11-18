169d83d9c5f46b9f6a67b8a37c7f6bbbded4aa7b
Cammy Smith <cjsarab1991@gmail.com>
2022-11-18 20:22:13 +0000

    Added enum roomtype

---
+++ src/main/java/RoomType.java
@@ -0,0 +1,22 @@
+public enum RoomType {
+    SINGLE(1, 1),
+    DOUBLE(2, 2),
+    TWIN(3, 2),
+    FAMILY(4, 4);
+
+    private final int value;
+    private final int capacity;
+
+    RoomType(int value, int capacity) {
+        this.value = value;
+        this.capacity = capacity;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+}
